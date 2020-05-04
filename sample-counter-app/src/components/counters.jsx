6892ee7ca7e388a287116e4512cbc8182889d807
Gadhe Bhavesh <bhavesh.gadhe@scania.com>
2020-05-04 14:16:35 +0200

    reactjs example

---
+++ sample-counter-app/src/components/counters.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.onReset} className="btn-primary btn-sm m-2">
+          Reset
+        </button>
+
+        {this.props.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            counter={counter}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
+          ></Counter>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
